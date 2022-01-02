62cdc9d89f68ade90f164a3f1c18f064b90994ef
91978 <91978@STADSOLUTION>
2022-01-02 16:26:37 +0530

    FirstCommit

---
+++ Jenkiens_ maven_with_testng_Example/src/main/java/jenkien/maven_with_testng_Example/App.java
@@ -0,0 +1,13 @@
+package jenkien.maven_with_testng_Example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Jenkiens_ maven_with_testng_Example/src/test/java/test/FirstProgram.java
@@ -0,0 +1,32 @@
+package test;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FirstProgram {
+
+	@Test
+	void method1()
+	{
+		System.out.println("inside method 1");
+		WebDriver driver;
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		System.out.println("after drive setup");
+		driver.get("https://www.google.com/");
+	}
+@Test
+void method2()
+{
+System.out.println("hello 2");
+}
+@Test
+void show()
+{
+System.out.println("in show");
+}
+}
